626e317a192a9d6109cfcdd701187da6be6e95eb
Marin Valenčak <valencakmarin@gmail.com>
2026-02-18 04:31:35 +0100

    Replace placeholder UI and add event handler to code-behind
    	- Replaces hard coded URL to a .png file with dynamic URLs to
    	  card pictures
    	- Implements VirtualizingStackPanel so only the pictures on
    	  screen are rendered
    	- Add OnListBoxLoaded event handler when the ListBox loads to
    	  the UI so that it can automatically download images and get
    	  card names

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,18 @@
 using Avalonia.Controls;
+using Heartstone_Archetype_Predictor.ViewModels;
+using System.Threading.Tasks;
 
 namespace Heartstone_Archetype_Predictor.Views;
 
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
-        InitializeComponent();
+public partial class MainWindow : Window {
+	public MainWindow() {
+		InitializeComponent();
+	}
+
+	private async void OnListBoxLoaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
+        if (DataContext is MainWindowViewModel viewModel)
+        {
+            await viewModel.OnListBoxLoaded();
+        }
     }
 }
