e2b0de6b52816e33321dba34d7359976297a199d
Marin Valenčak <valencakmarin@gmail.com>
2026-02-18 04:35:02 +0100

    Implement logic and data binding to view
    	- Add methods GetCardNames and GetImagesAsync
    	- GetCardNames searches through the ObservableCollection and
    	  retrives every card's name
    	- GetImagesAsync method retrieves images from
    	  HearthstoneSerializer class and exposes them to the UIsearches
    	  through the ObservableCollection and retrives every card's
    	  name
    	- GetImagesAsync method retrieves images from
    	  HearthstoneSerializer class and exposes them to the UI..

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,50 @@
-﻿namespace Heartstone_Archetype_Predictor.ViewModels;
+﻿using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using Heartstone_Archetype_Predictor.Models;
+using Heartstone_Archetype_Predictor.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
+namespace Heartstone_Archetype_Predictor.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged {
+
+    private readonly HearthstoneSerializer _hearthstoneSerializer;
+
+    public ObservableCollection<Bitmap> Images { get; private set; }
+
+    private ObservableCollection<string> _cardNames;
+
+    public ObservableCollection<string> CardNames {
+        get {
+            return _cardNames;
+        }
+    }
+
+    public virtual async Task OnListBoxLoaded() {
+        await GetImagesAsync();
+    }
+    public MainWindowViewModel() {
+        this._hearthstoneSerializer = new HearthstoneSerializer();
+        this.Images = new ObservableCollection<Bitmap>();
+        this._cardNames = new ObservableCollection<string>();
+    }
+
+    private void GetCardNames() {
+        foreach (var card in _hearthstoneSerializer.Cards) {
+            _cardNames.Add(card.Key.Name);
+        }
+    }
+    public async Task GetImagesAsync() {
+        List<Bitmap> images = await _hearthstoneSerializer.GetImagesAsync();
+
+        foreach (Bitmap image in images)
+        {
+            Images.Add(image);
+        }
+        GetCardNames();
+    }
 }
