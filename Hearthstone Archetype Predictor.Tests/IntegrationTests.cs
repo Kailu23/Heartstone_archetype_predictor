e0530a4bc28d5fb77eaf7d37271da9de40eda8e5
Marin Valenčak <valencakmarin@gmail.com>
2026-06-20 14:21:18 +0200

    test(integration): use POM for IntegrationTests.cs
        - 3 integration tests

--- Hearthstone Archetype Predictor.Tests/IntegrationTests.cs
+++ Hearthstone Archetype Predictor.Tests/IntegrationTests.cs
@@ -1,50 +1,61 @@
 using Hearthstone_Archetype_Predictor.Models;
+using Hearthstone_Archetype_Predictor.Tests.Helpers;
+using Hearthstone_Archetype_Predictor.Tests.PageObjects;
 using Xunit;
 
 namespace Hearthstone_Archetype_Predictor.Tests;
 
 public class HearthstoneSerializerIntegrationTests
 {
-    private const string WarriorDeckstring = "AAECAQcCrwSRvAIOHLACkQP/A44FqAXUBaQG7gbnB+8HgrACiLACub8CAAA=";
+    private const string WarriorDeckstring =
+        "AAECAQcCrwSRvAIOHLACkQP/A44FqAXUBaQG7gbnB+8HgrACiLACub8CAAA=";
 
-    private const string DruidDeckstring = "AAECAbSKAwrbnwSboASjoAS4oASD1ASY1ASZ1ASh1ASxmAX9xAUKh58EiZ8Erp8E2Z8E2p8EuaAE3KAEgdQEo9QEjaQFAAEB8L8E/cQFAdqhBf3EBQA=";
+    private const string DruidDeckstring =
+        "AAECAbSKAwrbnwSboASjoAS4oASD1ASY1ASZ1ASh1ASxmAX9xAUKh58EiZ8Erp8E2Z8E2p8EuaAE3KAEgdQEo9QEjaQFAAEB8L8E/cQFAdqhBf3EBQA=";
 
     [Fact]
     public async Task IsValidDeckstring_WarriorDeckstring_PassesDeserialization()
     {
-    //Given
-        var serializer = new HearthstoneSerializer(WarriorDeckstring);
-    //When
-        bool result  = serializer.IsValidDeckString(WarriorDeckstring);
-    //Then
+        //Given
+        var page = new SerializerPage(WarriorDeckstring);
+        //When
+        await page.WaitForNetworkDelay(100);
+        bool result = page.SubmitDeckString(WarriorDeckstring);
+        //Then
         Assert.True(result, "Warrior deck isn't valid");
     }
 
     [Fact]
-    public void IsValidDeckString_ChangingDeckstring_BothPassValidation()
+    public async Task IsValidDeckString_ChangingDeckstring_BothPassValidation()
     {
-        var serializer = new HearthstoneSerializer(WarriorDeckstring);
-    //When
-        bool result  = serializer.IsValidDeckString(WarriorDeckstring);
-    //Then
+        //Given
+        var page = new SerializerPage(WarriorDeckstring);
+        //When
+        bool result = page.SubmitDeckString(WarriorDeckstring);
+        //Then
         Assert.True(result, "Warrior deck isn't valid");
 
-        serializer.DeckString = DruidDeckstring;
-
-        result  = serializer.IsValidDeckString(DruidDeckstring);
-
+        await WaitHelper.WaitUntil(
+            condition: () => page.CurrentDeckString == WarriorDeckstring,
+            timeout: TimeSpan.FromSeconds(1)
+        );
+        //When
+        result = page.SubmitDeckString(DruidDeckstring);
+        //Then
         Assert.True(result, "Druid deck isn't valid");
+        Assert.Equal(DruidDeckstring, page.CurrentDeckString);
     }
 
     [Fact]
     public void IsValidDeckString_InvalidDeckstring_FailsDeserialization()
     {
-    //Given
-        var serializer = new HearthstoneSerializer();
-        const string InvalidDeckstring = "AAECAQcCrwSRvAIOHLACkQP/A44FqAXUBaQG7gbnB+8HgrACiLACub8CAAA=ddas";
-    //When
-        bool result = serializer.IsValidDeckString(InvalidDeckstring);
-    //Then
+        //Given
+        var page = new SerializerPage();
+        const string InvalidDeckstring =
+            "AAECAQcCrwSRvAIOHLACkQP/A44FqAXUBaQG7gbnB+8HgrACiLACub8CAAA=ddas";
+        //When
+        bool result = page.SubmitDeckString(InvalidDeckstring);
+        //Then
         Assert.False(result, "Invalid deck string should have failed deserialization.");
     }
 }
